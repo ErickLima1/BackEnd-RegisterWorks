e532929620b289e5b95b833c58449075399d6a22
ErickLima <ericklimacosta29@gmail.com>
2023-05-31 16:04:18 -0300

    Adicionando o passport onde o usuario sera auth

---
+++ backend/src/middlewares/passportValidations.js
@@ -0,0 +1,53 @@
+//Carretgando Modulos
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
+const User = require('../models/registerModels');
+const connection = require('../models/connection');
+
+//Configurando o passport e definir a estrategia de autenticação se der certo é GOD
+passport.use(new localStrategy({ usernameField: 'email', passwordField: 'senha'}, async (email, senha, done) => {
+    try {
+        //Aqui ele vai ver se existe um registro existe no banco
+        const query = 'SELECT * FROM registrar WHERE email = ?';
+        const [rows] = await connection.execute(query, [email]);
+        const user = rows[0];
+
+        if(!user) {
+            console.log('Email Usuario', email); //deletar
+            return done(null, false, {message: 'Email Não Encontrando.'});
+        }
+
+        //bcrypt compare senha
+        const isSenha = await bcrypt.compare(senha, user.senha);
+        if(!isSenha) {
+            console.log('Senha fornecida:', senha); //Deletar
+            console.log('Senha do usuário:', user.senha); //Deletar
+
+            return done(null, false, {message: 'Senha Incorreta'});
+        }
+        return done(null, user);
+
+    } catch(error) {
+        return done(error);
+    }
+}));
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+
+});
+
+passport.deserializeUser( async(id, done) => {
+    try{
+        const user = await User.findById(id);
+        done(null, user);
+        
+    }catch(error) {
+        done(error);
+    }
+
+});
+
+//exports para usar em outro lugar
+module.exports = passport;
