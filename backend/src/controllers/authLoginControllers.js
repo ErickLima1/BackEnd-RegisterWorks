3efee83306522d4b0dbf0d5a4f6a075898bb091d
ErickLima <ericklimacosta29@gmail.com>
2023-06-02 00:33:37 -0300

    Auth atualizado com bug resolvido

--- backend/src/controllers/authLoginControllers.js
+++ backend/src/controllers/authLoginControllers.js
@@ -2,21 +2,16 @@ const passport = require('../middlewares/passportValidations');
 
 //Definir a logica para login do usúario.
 const controllerLogin = (req, res, next) => {
-    passport.authenticate('local', (err, user, info) => {
+    passport.authenticate('local', {session: false}, (err, user, info) => {
         if(err) {
             return next(err);
         }
         if(!user) {
             return res.status(401).json({message: info.message});
         }
-        req.logIn(user, (err) => {
-            if(err) {
-                return next(err);
-            }
-            return res.json({message: 'Login Aprovado'});
-            
-        });
         
+        req.user = user;
+        next();
     })(req, res, next);
 };
 
