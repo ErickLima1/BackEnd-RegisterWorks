773178795a6400a5c026e828372c52a0b82c498b
ErickLima <ericklimacosta29@gmail.com>
2023-06-13 13:48:48 -0300

    Controle dos registro

--- backend/src/controllers/registerControllers.js
+++ backend/src/controllers/registerControllers.js
@@ -2,19 +2,21 @@
 const registerModel = require('../models/registerModels');
 
 
-const getAll = async (req, res) => {
+const getAllUsers = async (req, res) => {
     try {
-        const register = await registerModel.getAll(); 
-        return res.status(200).json(register);
+        const result = await registerModel.getAllUsers(); 
+
+        return res.status(200).json(result);
 
     }catch(error) {
         return res.status(500).json({message: 'Erro Interno no Servidor!'});
     }
 };
 
-const createRegister = async (req, res) => {
+const createRegisterUser = async (req, res) => {
     try {
-        const createdUser = await registerModel.createRegister(req.body);
+        const createdUser = await registerModel.createRegisterUser(req.body);
+
         return res.status(201).json(createdUser);
         
     }catch(error) {
@@ -26,6 +28,8 @@ const deleteUser = async(req, res) => {
     try {
         const {id} = req.params;
         await registerModel.deleteUser(id);
+        console.log(id);
+
         return res.status(204).json();
 
 
@@ -34,10 +38,9 @@ const deleteUser = async(req, res) => {
     }
 };
 
-
 //Importando Objetos
 module.exports = {
-    getAll,
-    createRegister,
+    getAllUsers,
+    createRegisterUser,
     deleteUser,
 };
