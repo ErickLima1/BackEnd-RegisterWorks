1a7c90569fcee2ff99ec5bfc66ad9e436d2c07c4
ErickLima <ericklimacosta29@gmail.com>
2023-07-09 17:24:15 -0300

    Servidor Rodando add swagger

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,11 @@
 const app = require('./app');
 require('dotenv').config();
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocs = require('./swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 const PORT = process.env.PORT || 8081;
 
 app.listen(PORT, () => {
