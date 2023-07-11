3c4af7003c5c436a4c8be7597526cef55beaf73c
ErickLima <ericklimacosta29@gmail.com>
2023-07-11 17:00:28 -0300

    Server

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,7 @@
 const app = require('./app');
 require('dotenv').config();
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocs = require('./swagger.json');
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+//
 
 const PORT = process.env.PORT || 8081;
 
